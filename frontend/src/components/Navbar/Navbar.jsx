30aa427de37c4a1226f7bd6a0bac9f5b7f25def1
riffathRHM <riffath.emedia@gmail.com>
2026-01-28 15:06:13 +0530

    update navbar.jsx for menu items

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <img src={assets.logo} alt="" className='logo'/>
+      <ul className="navbar-menu">
+        <li>home</li>
+        <li>menu</li>
+        <li>mobile-app</li>
+        <li>contact us</li>
+      </ul>
+
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="search-icon-icon">
+            <img src={assets.basket_icon} alt="" />
+            <div className="dot"></div>
+        </div>
+        <button>Sign In</button>
+      </div>
     </div>
   )
 }
